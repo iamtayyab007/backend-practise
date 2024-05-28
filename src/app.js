755b2dd0c4752e86d1807c55a250f82c3477869a
iamtayyab007 <tayyabm336@gmail.com>
2024-05-29 01:09:50 +0500

    add router and controller

--- src/app.js
+++ src/app.js
@@ -14,4 +14,13 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// Routes import
+import userRouter from "./routes/user.routes.js";
+
+// Routes declaration
+app.use("/api/v1/users", userRouter);
+
+//http://localhost:8000/api/v1/users/register
+
 export {app};
